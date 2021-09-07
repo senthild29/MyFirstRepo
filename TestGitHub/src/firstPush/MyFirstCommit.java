8c8743a0280e0643be562b1f41305366886d3163
Senthil <Senthil.dhanase@gmail.com>
2021-09-07 10:23:57 +0530

    Added Open and close Browser

--- TestGitHub/src/firstPush/MyFirstCommit.java
+++ TestGitHub/src/firstPush/MyFirstCommit.java
@@ -1,5 +1,16 @@
 package firstPush;
 
+import org.testng.annotations.Test;
+
 public class MyFirstCommit {
+	
+	@Test
+	public void OpenBrowser() {
+		//This method will open the browser
+	}
+	
+	public void CloseBrowser() {
+		//This method will close the browser
+	}
 
 }
